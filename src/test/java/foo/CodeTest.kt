77c9faae211135e0ad4365fb85bbf460ae0123d4
Kirill (KIR) Maximov <kir@maxkir.com>
2018-12-04 16:10:45 +0100

    Add more metadata to the test

--- src/test/java/foo/CodeTest.kt
+++ src/test/java/foo/CodeTest.kt
@@ -11,8 +11,19 @@ import org.testng.annotations.Test
 class CodeTest {
 
     fun test_ok() {
+
+        // Simple text metadata:
         println("##teamcity[testMetadata key='some key' value='some value']")
+
+        // Numeric metadata
         println("##teamcity[testMetadata key='a numeric value' type='number' value='${Code().value()}']")
+
+        // Reference to an artifact
+        // gradle_test_report.zip should be created by specifying corresponding artifact path in TC
+        val testsPath = "gradle_test_report.zip!/classes/${javaClass.name}.html"
+        println("##teamcity[testMetadata key='Gradle test report' type='artifact' value='$testsPath']")
+
+
     }
 
     fun test_failure() {
