948420fb877ad06da1d36cc2e1a46e62237d0491
kir <kir@TeamCity>
2018-12-04 16:04:50 +0100

    TeamCity change in 'test metadata' project: Versioned settings configuration updated

---
+++ .teamcity/settings.kts
@@ -0,0 +1,70 @@
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2018.2"
+
+project {
+
+    buildType(TestMetadataDemo)
+    buildType(BigKey)
+}
+
+object BigKey : BuildType({
+    name = "big key"
+
+    params {
+        param("teamcity.build.serviceMessages.logOriginal", "true")
+    }
+
+    steps {
+        script {
+            scriptContent = """
+                echo "##teamcity[testStarted name='failedTest']"
+                echo "##teamcity[testMetadata testName='failedTest' name='a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line a very long line' type='text' value='some text']"
+                echo "##teamcity[testFailed name='failedTest']"
+                echo "##teamcity[testFinished name='failedTest']"
+            """.trimIndent()
+        }
+    }
+})
+
+object TestMetadataDemo : BuildType({
+    name = "Test metadata demo"
+
+    artifactRules = "build/reports/tests/test => gradle_test_report.zip"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        gradle {
+            tasks = "clean build"
+            buildFile = ""
+            gradleWrapperPath = ""
+        }
+    }
+})
