9ee86498eb63fe881a64913a0f3b7c9f2004dcee
Kirill (KIR) Maximov <kir@maxkir.com>
2020-03-09 19:03:44 +0100

    Add VCS Trigger

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,9 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.project
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.version
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -38,6 +41,10 @@ object TestMetadataDemo : BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
+    }
+
+    triggers {
+        vcs {}
     }
 
     params {
